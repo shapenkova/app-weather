ca66894ecb5240b950d842289de28b667d066994
Anastasia Shapenkova <wondernasty@yandex.ru>
2024-10-29 12:03:47 +0500

    добавила фон при смене погодных условий

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="weather">
+  <div class="weather" :class="weatherClass">
     <div class="container">
       <div class="card weather-form">
         <input type="text" class="weather-form__input" placeholder="Enter city" v-model="searchQuery" @keyup.enter="weatherSearch">
@@ -21,11 +21,14 @@
     <div class="weather-bg">
       <div>
         <img class="weather-bg__img bg" src="./assets/bg.jpg" alt="App Background">
-        <!-- <img class="weather-bg__img overcast" src="./assets/overcast.jpg" alt="App Background">
+        <img class="weather-bg__img overcast" src="./assets/overcast.jpg" alt="Overcast">
         <img class="weather-bg__img partly-cloudy" src="./assets/partly-cloudy.jpg" alt="Partly Cloudy">
-        <img class="weather-bg__img sunny" src="./assets/sunny.jpg" alt="Sunny"> -->
+        <img class="weather-bg__img sunny" src="./assets/sunny.jpg" alt="Sunny">
+        <img class="weather-bg__img light-snow" src="./assets/snow.jpg" alt="Light snow">
+        <img class="weather-bg__img light-rain" src="./assets/rain.jpg" alt="Light rain">
       </div>
     </div>
+
   </div>
 </template>
 
@@ -44,7 +47,21 @@ export default {
   },
 
   computed: {
-
+    weatherClass() {
+      if (this.description.includes('Sunny')) {
+        return 'sunny';
+      } else if (this.description.includes('Overcast')) {
+        return 'overcast';
+      } else if (this.description.includes('Partly cloudy')) {
+        return 'partly-cloudy';
+      } else if (this.description.includes('Light snow')) {
+        return 'light-snow';
+      }  else if (this.description.includes('Light rain')) {
+        return 'light-rain';
+      } else {
+        return '';
+      }
+    }
   },
 
   methods: {
@@ -59,6 +76,7 @@ export default {
           this.temperature = data.current.temp_c;
           this.description = data.current.condition.text;
           this.resetSearchQuery();
+          console.log(data);
       })
       .catch(error => {
         this.loading = false;
