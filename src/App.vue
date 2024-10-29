0ae6cb8df3d216cae8420ca9409cf32638fd8ae9
Anastasia Shapenkova <wondernasty@yandex.ru>
2024-10-29 19:39:33 +0500

    подкорректировала наименование классов и стили

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,30 @@
 <template>
   <div class="weather" :class="weatherClass">
-    <div class="container">
-      <div class="card weather-form">
-        <input type="text" class="weather-form__input" placeholder="Enter city" v-model="searchQuery" @keyup.enter="weatherSearch">
-        <button class="weather-form__btn" @click="weatherSearch">Search</button>
+    <div class="weather__container">
+      <div class="card weather__form form">
+        <input type="text" class="form__input" placeholder="Enter city" v-model="searchQuery" @keyup.enter="weatherSearch">
+        <button class="form__btn" @click="weatherSearch">Search</button>
       </div>
 
-      <div class="card weather-load" v-if="loading">Loading...</div>
-      <div class="weather-info" v-show="!error && location && temperature !== 0 && description">
+      <div class="card weather__load" v-if="loading">Loading...</div>
+      <div class="weather__info" v-show="!error && location && temperature !== 0 && description">
         <div class="card" v-if="error">Error</div>
-        <div class="weather-info__text">
+        <div class="weather__info-text">
           <p class="card">{{ location }}</p>
           <p class="card">{{ temperature }} °{{ unit }}</p>
           <p class="card">{{ description }}</p>
         </div>
       </div>
-
     </div>
 
-    <div class="weather-bg">
+    <div class="weather__bg">
       <div>
-        <img class="weather-bg__img bg" src="./assets/bg.jpg" alt="App Background">
-        <img class="weather-bg__img overcast" src="./assets/overcast.jpg" alt="Overcast">
-        <img class="weather-bg__img partly-cloudy" src="./assets/partly-cloudy.jpg" alt="Partly Cloudy">
-        <img class="weather-bg__img sunny" src="./assets/sunny.jpg" alt="Sunny">
-        <img class="weather-bg__img light-snow" src="./assets/snow.jpg" alt="Light snow">
-        <img class="weather-bg__img light-rain" src="./assets/rain.jpg" alt="Light rain">
+        <img class="weather__bg-img bg" src="./assets/bg.jpg" alt="App Background">
+        <img class="weather__bg-img overcast" src="./assets/overcast.jpg" alt="Overcast">
+        <img class="weather__bg-img partly-cloudy" src="./assets/partly-cloudy.jpg" alt="Partly Cloudy">
+        <img class="weather__bg-img sunny" src="./assets/sunny.jpg" alt="Sunny">
+        <img class="weather__bg-img light-snow" src="./assets/snow.jpg" alt="Light snow">
+        <img class="weather__bg-img light-rain" src="./assets/rain.jpg" alt="Light rain">
       </div>
     </div>
 
@@ -43,24 +42,24 @@ export default {
       loading: false,
       error: false,
       unit: 'C',
+      weatherTypes: {
+        'Sunny': 'sunny',
+        'Overcast': 'overcast',
+        'Partly cloudy': 'partly-cloudy',
+        'Light snow': 'light-snow',
+        'Light rain': 'light-rain',
+      },
     };
   },
 
   computed: {
     weatherClass() {
-      if (this.description.includes('Sunny')) {
-        return 'sunny';
-      } else if (this.description.includes('Overcast')) {
-        return 'overcast';
-      } else if (this.description.includes('Partly cloudy')) {
-        return 'partly-cloudy';
-      } else if (this.description.includes('Light snow')) {
-        return 'light-snow';
-      }  else if (this.description.includes('Light rain')) {
-        return 'light-rain';
-      } else {
-        return '';
+      for (const [key, value] of Object.entries(this.weatherTypes)) {
+        if (this.description.includes(key)) {
+          return value;
+        }
       }
+      return '';
     }
   },
 
@@ -76,7 +75,6 @@ export default {
           this.temperature = data.current.temp_c;
           this.description = data.current.condition.text;
           this.resetSearchQuery();
-          console.log(data);
       })
       .catch(error => {
         this.loading = false;
